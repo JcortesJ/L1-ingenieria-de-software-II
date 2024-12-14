9aaaf96841a36f6353e32d9330f45b2593d9192a
jcortesj <juancortes@habi.co>
2024-12-14 15:01:23 -0500

    added landing page

--- l1-ingesoft/src/app/queryPage/page.tsx
+++ l1-ingesoft/src/app/queryPage/page.tsx
@@ -1,31 +1,48 @@
-
-import DepartamentoTab from "@/components/DepartamentoTab"
-import MunicipioTab from "@/components/MunicipioTab"
-import PersonaTab from "@/components/PersonaTab"
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
-import ViviendaTab from "@/components/ViviendaTab"
-
+import DepartamentoTab from "@/components/DepartamentoTab";
+import MunicipioTab from "@/components/MunicipioTab";
+import PersonaTab from "@/components/PersonaTab";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import ViviendaTab from "@/components/ViviendaTab";
 
 const queryPage = () => {
-    return (
-        <div className="w-full h-full p-16">
-            <h1 className="text-3xl font-bold mb-4 ">Realice las consultas y observe su resultado</h1>
-            <Tabs defaultValue="Personas" className="w-full ">
-                <TabsList className="w-full flex bg-[#546057] text-white ">
-                    <TabsTrigger className="flex-1 data-[state=active]:bg-[#C1C8C1] " value="Personas">Personas</TabsTrigger>
-                    <TabsTrigger className="flex-1" value="Viviendas">Viviendas</TabsTrigger>
-                    <TabsTrigger  className="flex-1" value="Municipios">Municipios</TabsTrigger>
-                    <TabsTrigger className="flex-1" value="Departamentos">Departamentos</TabsTrigger>
-                </TabsList>
-                <TabsContent value="Personas"><PersonaTab/></TabsContent>
-                <TabsContent value="Viviendas"><ViviendaTab/></TabsContent>
-                <TabsContent value="Municipios"><MunicipioTab/></TabsContent>
-                <TabsContent value="Departamentos"><DepartamentoTab/></TabsContent>
+  return (
+    <div className="w-full h-full p-16">
+      <h1 className="text-3xl font-bold mb-4 ">
+        Realice las consultas y observe su resultado
+      </h1>
+      <Tabs defaultValue="Personas" className="w-full ">
+        <TabsList className="w-full flex bg-[#546057] text-white ">
+          <TabsTrigger
+            className="flex-1 data-[state=active]:bg-[#C1C8C1] "
+            value="Personas"
+          >
+            Personas
+          </TabsTrigger>
+          <TabsTrigger className="flex-1" value="Viviendas">
+            Viviendas
+          </TabsTrigger>
+          <TabsTrigger className="flex-1" value="Municipios">
+            Municipios
+          </TabsTrigger>
+          <TabsTrigger className="flex-1" value="Departamentos">
+            Departamentos
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="Personas">
+          <PersonaTab />
+        </TabsContent>
+        <TabsContent value="Viviendas">
+          <ViviendaTab />
+        </TabsContent>
+        <TabsContent value="Municipios">
+          <MunicipioTab />
+        </TabsContent>
+        <TabsContent value="Departamentos">
+          <DepartamentoTab />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
 
-            </Tabs>
-        </div>
-
-    )
-}
-
-export default queryPage
+export default queryPage;
