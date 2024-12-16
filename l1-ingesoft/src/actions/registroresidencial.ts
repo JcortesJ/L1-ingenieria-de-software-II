f5f740ecc189809fadabf28c8598cb021c28ddd2
jcortesj <juancortes@habi.co>
2024-12-16 02:04:58 -0500

    added registro residencial create action

--- l1-ingesoft/src/actions/registroresidencial.ts
+++ l1-ingesoft/src/actions/registroresidencial.ts
@@ -1,6 +1,6 @@
 export const fetchRegistroResidencial = async () => {
   try {
-    const response = await fetch("/api/registroresidencial", {
+    const response = await fetch("/api/registro", {
       method: "GET",
     });
 
@@ -17,4 +17,19 @@ export const fetchRegistroResidencial = async () => {
       error: "No se pudo cargar los datos de registro residencial.",
     };
   }
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const createRegistroResidencial = async (data: any) => {
+  const response = await fetch("/api/registro/create", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al crear el registro residencial");
+  }
+
+  const responseData = await response.json();
+  return { responseData, error: null };
 };
